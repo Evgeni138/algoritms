95b36b59082c3360092f6b38b6683fbd8bd3fca4
Evgegeni <popovevgeni81@gmail.com>
2023-01-16 21:59:56 +0800

    Lection2_v2

--- src/main/java/lesson1/Fibonachi.java
+++ src/main/java/lesson1/Fibonachi.java
@@ -7,7 +7,7 @@ public class Fibonachi {
         counter.incrementAndGet();
         if (index == 0) return 0;
         if (index == 1) {
-            return 0;
+            return 1;
         }if (index == 2) {
             return 1;
         }
