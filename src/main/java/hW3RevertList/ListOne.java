298bf5f2dae0fb91c8748a43c126fb0b411c1f90
Evgegeni <popovevgeni81@gmail.com>
2023-01-22 19:23:28 +0800

    HW3RevertList_v4

--- src/main/java/hW3RevertList/ListOne.java
+++ src/main/java/hW3RevertList/ListOne.java
@@ -3,6 +3,7 @@ package hW3RevertList;
 public class ListOne {
     NodeOne head;
 
+    // Добавление элемента
     public void push(int value) {
         NodeOne node = new NodeOne();
         node.value = value;
@@ -10,6 +11,7 @@ public class ListOne {
         head = node;
     }
 
+    // Вывод всех эдементов в консоль
     public void printAll() {
         NodeOne nodeOne = head;
         while (nodeOne != null) {
@@ -18,5 +20,34 @@ public class ListOne {
         }
     }
 
+    // Рекурсивный разворот
+    public void revert() {
+        if (head != null && head.next != null) {
+            NodeOne temp = head;
+            revert(head.next, head);
+            temp.next = null;
+        }
+    }
 
+
+    // Разворот через создание нового списка
+    public ListOne revert2() {
+        NodeOne node = new NodeOne();
+        ListOne newList = new ListOne();
+        node = head;
+        while (node != null) {
+            newList.push(node.value);
+            node = node.next;
+        }
+        return newList;
+    }
+
+    private void revert(NodeOne currentNode, NodeOne previousNode) {
+        if (currentNode.next == null) {
+            head = currentNode;
+        } else {
+            revert(currentNode.next, currentNode);
+        }
+        currentNode.next = previousNode;
+    }
 }
--- src/main/java/hW3RevertList/Program.java
+++ src/main/java/hW3RevertList/Program.java
@@ -1,5 +1,5 @@
 package hW3RevertList;
-
+// Необходимо реализовать метод разворота связного списка
 public class Program {
     public static void main(String[] args) {
         List list1 = new List();
@@ -8,11 +8,12 @@ public class Program {
         list1.push(3);
         list1.push(4);
         list1.push(5);
+        System.out.println("Двусвязный список: ");
         list1.printAll();
-        System.out.println();
+        System.out.println("Разворот через создание нового списка");
         list1 = list1.revert();
         list1.printAll();
-        System.out.println();
+        System.out.println("Рекурсивный разворот");
         list1.revert2();
         list1.printAll();
 
@@ -25,6 +26,12 @@ public class Program {
         listOne.push(4);
 
         System.out.println("Односвязанный список: ");
+        listOne.printAll();
+        System.out.println("Рекурсивный разворот");
+        listOne.revert();
+        listOne.printAll();
+        System.out.println("Разворот через создание нового списка");
+        listOne = listOne.revert2();
         listOne.printAll();
     }
 }
